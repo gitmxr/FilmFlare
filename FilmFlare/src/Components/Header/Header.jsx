b2d5fe4864d92351bb5a06a2ee1303008a49c597
Your Name <riazdev18@gmail.com>
2025-07-28 04:00:41 +0500

    updated

--- FilmFlare/src/Components/Header/Header.jsx
+++ FilmFlare/src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "/src/assets/images/filmflare-logo.webp";
+import logo from "/src/assets/images/filmflare_logo.webp";
 
 function Header({ searchQuery, setSearchQuery }) {
   const handleSearch = () => {
